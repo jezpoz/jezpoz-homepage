316e2c3a1adf6ee574ac7213e8d508099f4489a6
jezpoz <jesfor94@gmail.com>
2024-09-03 14:07:59 +0200

    remove loadEnv from vite since sanity project-id isnt sensitive

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,20 +4,13 @@ import netlify from '@astrojs/netlify';
 import sanity from '@sanity/astro';
 
 import react from '@astrojs/react';
-import { loadEnv } from "vite";
-import { resolve } from 'path';
-
-const {
-  SANITY_PROJECT_ID,
-  SANITY_DATASET_NAME,
-} = loadEnv(process.env.NODE_ENV, resolve(process.cwd(), '..', '..'), 'SANITY');
 
 export default defineConfig({
   site: 'https://jezpoz-astro.netlify.app',
   integrations: [
     sanity({
-      projectId: SANITY_PROJECT_ID,
-      dataset: SANITY_DATASET_NAME,
+      projectId: 'rz9y3u62',
+      dataset: 'production',
       useCdn: false,
       apiVersion: "2024-09-03",
     }),
